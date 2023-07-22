2908d08708cb74313d8aa0e793543f6627ef5115
aditirajaraman10272008 <aditirajaraman10272008@gmail.com>
2023-07-21 22:30:47 -0400

    Caluclator First version

---
+++ calculator.py
@@ -0,0 +1,36 @@
+# # print ("enter a")
+#
+# val1 = input("Enter number: ")
+# a = 12
+# b = 19
+# add = int(val1) + b
+# sub = b - int(val1)
+# multiply = a * int(val1)
+#
+# print(a)
+# print(b)
+# print(add)
+# print(sub)
+# print(multiply)
+
+#version 2
+
+t = int(input("enter first number: "))
+h = int(input("enter second number: "))
+operator = input("what operation: ")
+
+# a - Add
+# s - Subtract
+# m - multiply
+
+if operator == "a":
+    result = t + h
+elif operator == "s":
+    result = h - t
+elif operator == "m":
+    result = t * h
+else:
+    result = -1
+    print("wrong operator")
+
+print("result: " + str((result)))
