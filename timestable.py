0e4814fd906ff42a465735ff579538541412c550
aditirajaraman10272008 <aditirajaraman10272008@gmail.com>
2023-07-22 22:42:48 -0400

    times table

---
+++ timestable.py
@@ -0,0 +1,7 @@
+multiple = int(input("Which multiplication table would you like?"))
+
+print("Here's your table:")
+
+for looper in range(1,11):
+    result = multiple * looper
+    print (looper, "* 5= ", looper * 5)
