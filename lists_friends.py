04d7cd553eb0a2b8c335e15011b8ebc9cba46e8a
aditirajaraman10272008 <aditirajaraman10272008@gmail.com>
2023-08-05 20:33:03 -0400

    version 1
    8/5/23

---
+++ lists_friends.py
@@ -0,0 +1,14 @@
+friends = []
+print('Enter five names: ')
+for looper in [1, 2, 3, 4, 5]:
+    friend = input()
+    friends.append(friend)
+
+print('The names are ' + friends[0] + " " + friends[1] + " " + friends[2])
+
+#friends = ["dog", "cat", "hamster", "dolphin","fish"]
+#print(friends[0])
+#print(friends[1])
+#print(friends[2])
+#print(friends[3])
+#print(friends[4])
