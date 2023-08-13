a4a2eef771e9b72bf3f1ce1036c90348a3b2bade
aditirajaraman10272008 <aditirajaraman10272008@gmail.com>
2023-08-13 18:50:53 -0400

    Functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+name = input("What is your name?")
+address = input("Address:")
+street = input("Street:")
+city = input("City:")
+state = input("State:")
+def locationInfo(name, address, street, city, state):
+    print(name)
+    print(address)
+    print(street)
+    print(city)
+    print(state)
+
+locationInfo(name, address, street, city, state)
